5a18b019530e83577e7d3213483314f510250729
Matias <matiasezequielsmania@gmail.com>
2024-12-04 18:18:32 -0300

    merge posta

--- Front/build/js/index.js
+++ Front/build/js/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = "https://sp-prograiii-fj7g.onrender.com";
 
 // Función para mostrar mensajes en pantalla
 function displayMessage(elementId, message, color) {
